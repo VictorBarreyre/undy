73571d9ff1dc888b5e88019e61d3221bd27dc5f7
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-10-24 12:36:23 +0200

    dependancies

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { NativeBaseProvider, Box } from 'native-base';
+import React, { useContext } from 'react';
+import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AuthProvider, AuthContext } from './context/AuthContext';
 import Register from './components/Register';
 import Login from './components/Login';
-import Home from './screens/Home';
+import DrawerNavigator from './Layout';  // Import du DrawerNavigator
 
 const Stack = createStackNavigator();
 
@@ -14,22 +14,28 @@ export default function App() {
         <AuthProvider>
             <NativeBaseProvider>
                 <NavigationContainer>
-                    <AuthContext.Consumer>
-                        {({ isLoggedIn }) => (
-                            <Stack.Navigator>
-                                {isLoggedIn ? (
-                                    <Stack.Screen name="Home" component={Home} />
-                                ) : (
-                                    <>
-                                        <Stack.Screen name="Register" component={Register} />
-                                        <Stack.Screen name="Login" component={Login} />
-                                    </>
-                                )}
-                            </Stack.Navigator>
-                        )}
-                    </AuthContext.Consumer>
+                    <StackNavigator />
                 </NavigationContainer>
             </NativeBaseProvider>
         </AuthProvider>
     );
 }
+
+const StackNavigator = () => {
+    const { isLoggedIn } = useContext(AuthContext);  // Vérifier si l'utilisateur est connecté
+
+    return (
+        <Stack.Navigator>
+            {isLoggedIn ? (
+                // Si l'utilisateur est connecté, afficher le DrawerNavigator
+                <Stack.Screen name="App" component={DrawerNavigator} options={{ headerShown: false }} />
+            ) : (
+                // Si l'utilisateur n'est pas connecté, afficher Login/Register
+                <>
+                    <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+                    <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+                </>
+            )}
+        </Stack.Navigator>
+    );
+};
