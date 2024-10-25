f9f43c799ee5f35ff1faa565e7ab394bab452428
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-25 11:56:46 +0200

    cors config

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+// Configuration CORS
+app.use(cors({
+  origin: ['http://localhost:8081', 'https://undy-93a12c731bb4.herokuapp.com'], // Ajoute d'autres domaines si nécessaire
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Précise les méthodes autorisées
+  allowedHeaders: ['Content-Type', 'Authorization'] // Précise les en-têtes autorisés
+}));
 app.get('/', (req, res) => {
     res.send('Why are you here man?');
   });
