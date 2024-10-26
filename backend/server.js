3736165a67a7bbe5e9501743dc64d9ac485f1153
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-26 10:02:22 +0200

    putain

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,7 @@ app.get('/', (req, res) => {
 
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
 })
 .then(() => console.log('MongoDB connecté'))
 .catch(err => console.error(err));
