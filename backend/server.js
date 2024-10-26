f6fafeeceb069a94a8232b15c81ab2c85669d73c
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-26 12:10:19 +0200

    config cors

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,33 @@
-// server.jss
+// server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+// Charger les variables d'environnement
 dotenv.config();
 
+// Initialiser l'application Express
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware pour analyser les requêtes JSONs
+// Middleware pour analyser les requêtes JSON
 app.use(express.json());
 
-// Configuration CORS avec options dynamiques pour autoriser les requêtes depuis les origines spécifiées
-const allowedOrigins = ['http://localhost:8081', 'https://undy-5948c5547ec9.herokuapp.com'];
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin); // Autorise l'origine de la requête
-    }
+// Configuration des options CORS
+const corsOptions = {
+    origin: [
+        'http://localhost:8081', 
+        'https://undy-5948c5547ec9.herokuapp.com'
+    ],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    credentials: true
+};
 
-    // Définir les en-têtes CORS
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-
-    // Répondre aux requêtes préflight immédiatement avec un statut 200
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
-
+// Appliquer CORS avec les options configurées
+app.use(cors(corsOptions));
 
 // Import des routes
 const userRoutes = require('./routes/userRoutes');
@@ -44,9 +40,11 @@ app.get('/', (req, res) => {
 
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connecté'))
-.catch(err => console.error(err));
+.catch(err => console.error('Erreur de connexion MongoDB:', err));
 
 // Démarrer le serveur
 app.listen(PORT, () => {
