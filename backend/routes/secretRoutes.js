b89c7cf9c9a0ad8272830dbf84bef707c2bd5012
Vr <vr@MacBook-Pro-de-Vr.local>
2025-01-11 11:25:31 +0100

    trble shot

--- backend/routes/secretRoutes.js
+++ backend/routes/secretRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createSecret, getAllSecrets, purchaseSecret, getUserSecretsWithCount, getSecret } = require('../controllers/secretController');
+const { createSecret, getAllSecrets, purchaseSecret, getUserSecretsWithCount } = require('../controllers/secretController');
 const protect  = require('../middleware/authMiddleware');
+const Secret = require('../models/Secret');
+
 
 // Routes publiques
 router.get('/', getAllSecrets); // Récupérer tous les secrets
@@ -9,9 +11,10 @@ router.get('/', getAllSecrets); // Récupérer tous les secrets
 // Routes privées
 router.post('/createsecrets', protect, createSecret); // Créer un secret
 router.post('/:id/purchase', protect, purchaseSecret); // Acheter un secret
-router.get('/:id', protect, getSecret); // Voir un secret acheté
 
-// Nouvelle route combinée pour récupérer les secrets de l'utilisateur et leur nombre
 router.get('/user-secrets-with-count', protect, getUserSecretsWithCount);
+
+
+
 
 module.exports = router;
